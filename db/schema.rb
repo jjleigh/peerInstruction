3c4278f6e5ccb190d4e88d190009227e3717f08f
git-justinpodur <git-justin@podur.org>
2014-07-22 15:07:37 -0400

    added is_open to questions table with default => true

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140722185801) do
+ActiveRecord::Schema.define(version: 20140722190047) do
 
   create_table "authentications", force: true do |t|
     t.integer  "user_id",    null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20140722185801) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "question"
+    t.boolean  "is_open",    default: true
   end
 
   create_table "users", force: true do |t|
