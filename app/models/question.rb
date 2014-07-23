6ac503de4ad0a1371d94f6ecd86c3f71c3a96823
Justine Jones <justinejones91@gmail.com>
2014-07-22 20:11:27 -0400

    finished setting up views for the questions model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ActiveRecord::Base
 	has_many :answer_choices
-	accepts_nested_attributes_for :answer_choices, reject_if => :all_blank, :allow_destroy => true
+	accepts_nested_attributes_for :answer_choices, :reject_if => :all_blank, :allow_destroy => true
 
 	validates :question, :presence => true 
 end
