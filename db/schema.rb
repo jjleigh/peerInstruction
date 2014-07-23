11d9f10f68f73e9ad05fd19f5bf0023a3fda3193
git-justinpodur <git-justin@podur.org>
2014-07-23 15:47:46 -0400

    merged and working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20140723191214) do
 
   create_table "answer_choices", force: true do |t|
