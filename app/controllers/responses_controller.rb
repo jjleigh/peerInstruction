aae66ca8dcbd94944654985114d13a863e3dbef9
git-justinpodur <git-justin@podur.org>
2014-07-24 16:00:39 -0400

    got responses working

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,9 +1,9 @@
 class ResponsesController < ApplicationController
 
   def create
-  	@answer_choice = Answer_choice.find(params[:answer_choice_id])
+  	@answer_choice = AnswerChoice.find(params[:answer_choice_id])
   	@answer_choice.users<<current_user
-  		if @answer_choice.save 
+  	if @answer_choice.save 
   		redirect_to questions_url, :notice => "Successfully answered"
   	else 
   		render 'questions/show'
