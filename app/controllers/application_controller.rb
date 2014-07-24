942a3b941d027e5945d7a0c4a0cd107d32a9f741
Justine Jones <justinejones91@gmail.com>
2014-07-24 19:16:22 -0400

    added the current user medthod to the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   private
-  
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
   def not_authenticated
     redirect_to login_path, alert: "Please login first"
   end
