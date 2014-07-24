58e118792b26a750ecb3282be29fdddcdee19f38
git-justinpodur <git-justin@podur.org>
2014-07-24 17:10:00 -0400

    question is voteable if open and not voteable otherwise

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,5 @@ class Question < ActiveRecord::Base
 	has_many :answer_choices
 	accepts_nested_attributes_for :answer_choices, :reject_if => :all_blank, :allow_destroy => true
 
-	validates :question, :is_open, :presence => true 
+	validates :question, :presence => true 
 end
