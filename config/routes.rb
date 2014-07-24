93384219289b2d9293e918dc4319f84390944912
git-justinpodur <git-justin@podur.org>
2014-07-24 15:09:43 -0400

    responses controller completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  resources :responses
-
   resources :password_resets
 
   resources :user_sessions
@@ -9,8 +7,10 @@ Rails.application.routes.draw do
   resources :users, :except => [:index]
 
   resources :questions do
-    resources :answer_choices
-  end 
+    resources :answer_choices do
+        resources :responses, :only => [:create]
+    end 
+  end
 
   root to: "welcome#index"
 
