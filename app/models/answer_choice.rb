a698b2cb9e39f9b7634eb1c98d104f3449ad0995
Justine Jones <justinejones91@gmail.com>
2014-07-25 19:35:06 -0400

    modified the assosiations to has many through

--- app/models/answer_choice.rb
+++ app/models/answer_choice.rb
@@ -1,24 +1,7 @@
 class AnswerChoice < ActiveRecord::Base
 	belongs_to :question
-	has_and_belongs_to_many :users
 
-# need to create a custom validation to prevent a user from voting twice
-	# after_vote :validate_number_of_responses
-	# validate :validate_number_of_responses
-
-	# private
-	# def validate_number_of_responses
-	
-	# 	answers_belongs_to_current_questions = AnswerChoice.where(question_id: question.id)
-	# 	answers_belongs_to_current_questions.each{|answer|
-	# 		answer.users.each{|user|
-	# 			if user.id == users[0].id
-	# 					puts "Error"
-	# 					errors.add(:users, "You have already voted")
-				
-	# 			end
-	# 		}
-	# 	}
-	# end
+	has_many :responses
+	has_many :users, through: :responses
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ActiveRecord::Base
   validates :name, presence: true
   validates :email, uniqueness: true
 
-  has_and_belongs_to_many :answer_choices
+  has_many :responses
 
 end
