7269127f9185ec326d4d2e6587a14b03ee1c69bb
Justine Jones <justinejones91@gmail.com>
2014-07-25 20:07:39 -0400

    made these commts already

--- db/migrate/20140725202421_create_responses.rb
+++ db/migrate/20140725202421_create_responses.rb
@@ -1,6 +1,9 @@
 class CreateResponses < ActiveRecord::Migration
   def change
     create_table :responses do |t|
+      t.integer :question_id
+      t.integer :answer_choice_id
+      t.integer :user_id
 
       t.timestamps
     end
