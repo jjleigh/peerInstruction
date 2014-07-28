151a91ca7f2b50c4286a38cc24f0d1c6988f5901
Justine Jones <justinejones91@gmail.com>
2014-07-28 15:03:35 -0400

    nothing happened here

--- app/models/response.rb
+++ app/models/response.rb
@@ -3,16 +3,18 @@ class Response < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :answer_choices
 
-#this does the same thing as the method below. Just saving it as an example of a secon way to do it.x
-
-	# validates :user_id, :uniqueness => {:scope => :question_id, :message => "You have already placed a vote"}
 
 
-	validate :user_can_only_vote_once
+	validates :user_id, :uniqueness => {:scope => :question_id, :message => "You have already placed a vote"}
 
-	def user_can_only_vote_once
-		if Response.where(:question_id => question_id).where(:user_id => user_id)
-			errors.add("You have already placed a vote")
-		end
-	end
+
+#this does the same thing as the method below. Just saving it as an example of a secon way to do it.
+
+	# validate :user_can_only_vote_once
+
+	# def user_can_only_vote_once
+	# 	if Response.where(:question_id => question_id).where(:user_id => user_id)
+	# 		errors.add(:response, "You have already placed a vote")
+	# 	end
+	# end
 end
