7a73ecad835fd28fb93425082befeaa4fdaad0ad
Justine Jones <justinejones91@gmail.com>
2014-07-28 13:38:44 -0400

    created the custom validation method which will prevent double voting

--- app/models/response.rb
+++ app/models/response.rb
@@ -3,48 +3,16 @@ class Response < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :answer_choices
 
-# first try 
-	# validates :user_id, uniqueness: true
+#this does the same thing as the method below. Just saving it as an example of a secon way to do it.x
+
+	# validates :user_id, :uniqueness => {:scope => :question_id, :message => "You have already placed a vote"}
 
 
-# second try - throws an error due to params not being accessible in the model
-	# validates :response, :respond_once => false
-	# def respond_once
-	# 	@response = Response.find(params[:id])
+	validate :user_can_only_vote_once
 
-	# 	if @response.question_id.user_id.exist?
-	# 		errors.add("You have already placed a vote")
-	# 	end
-	# end
-
-# third try - this also doesnt work
-	# validates :response, :user_can_only_respond_once => false
-	# def user_can_only_respond_once
-	# 	Response.each do |r|
-	# 		if r.question_id.user_id.exist?
-	# 			errors.add("You have already placed a vote")
-	# 		end
-	# 	end	
-	#  end
-
-
-# fourth try - this doesnt return an error but it doesnt work.
-	# validates :response, :respond_once => false
-	# 	def respond_once
-	# 		return Response.find(:first,:conditions => {:question_id => question_id, :user_id => user_id})
-	# 	end
-# fifth try - 
-	# class MyValidator < ActiveModel::Validator
-	# 	def validate
-	# 		@response.each do |r|
-	# 		if r.question_id.user_id.exist?
-	# 			errors.add("You have already placed a vote")
-	# 		end
-	# 	end
-# sixth try
-
-	# include ActiveModel::EachValidatior
-
-	# validates_with ResponseValidator
-	# end 
+	def user_can_only_vote_once
+		if Response.where(:question_id => question_id).where(:user_id => user_id)
+			errors.add("You have already placed a vote")
+		end
+	end
 end
