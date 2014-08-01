31a8bb204208d8466a21438f99f11373c9844515
Justine Jones <justinejones91@gmail.com>
2014-08-01 17:23:00 -0400

    added  config serve static assests to config folder

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,8 @@ module PeerInstruction
 
     # For Foundation 5
     config.assets.precompile += %w( vendor/modernizr )
-    
+    config.serve_static_assets = true
+    config.assets.compile = true
     
   end
 end
