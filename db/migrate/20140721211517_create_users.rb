e5bb0d376503685473e5cbcf0a408e541a9a0ef9
Justine Jones <justinejones91@gmail.com>
2014-08-01 16:43:21 -0400

    Reset migrations yet again

--- db/migrate/20140721211517_create_users.rb
+++ db/migrate/20140721211517_create_users.rb
@@ -1,7 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.timestamps
+    end
 
+    create_table :responses do |t|
       t.timestamps
     end
 
