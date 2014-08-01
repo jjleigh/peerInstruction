71f83ba3482c8a8f29891d256c63e12f7798e911
Justine Jones <justinejones91@gmail.com>
2014-08-01 10:33:00 -0400

    Edit initial users migration

--- db/migrate/20140721211517_create_users.rb
+++ db/migrate/20140721211517_create_users.rb
@@ -1,4 +1,14 @@
 class CreateUsers < ActiveRecord::Migration
   def change
+  	drop_table :users
+    create_table :users do |t|
+      t.string :email,            :null => false
+      t.string :crypted_password, :null => false
+      t.string :salt,             :null => false
+
+      t.timestamps
+    end
+
+    add_index :users, :email, unique: true
   end
 end
